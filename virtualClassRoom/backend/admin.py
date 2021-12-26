a54941298b827cd24bd64cfe404433259e92b575
maazbin <maaz.mstqm@gmail.com>
2021-12-27 03:45:10 +0500

    tables created except Tasks

--- virtualClassRoom/backend/admin.py
+++ virtualClassRoom/backend/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
-from backend.models import User
+from django.db import models
+import backend.models as tables
 
-admin.site.register(User)
+models_list = {
+    'User': tables.User,
+    # tables.Rooms,
+    # tables.Discussions,
+    # tables.Topics,
+    # tables.Responses,
+    # tables.Comments
+}
+
+
+admin.site.register(models_list['User'])
+# admin.site.register(tables.Rooms)
+# admin.site.register(tables.Discussions)
+# admin.site.register(tables.Topics)
+# admin.site.register(tables.Responses)
+# admin.site.register(tables.Comments)
