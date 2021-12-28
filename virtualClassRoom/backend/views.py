fd1f30ef92dba3fc6d55b0261855d364b49af558
maazbin <maaz.mstqm@gmail.com>
2021-12-29 01:43:29 +0500

    API List updated with more APIs

--- virtualClassRoom/backend/views.py
+++ virtualClassRoom/backend/views.py
@@ -5,21 +5,27 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 		# Model Imports 
-from .models import User
+from .models import User,Room,Discussion
+
+        # serializers Imports
 from backend import serializers
-from .serializers import UserSerializer
+from .serializers import UserSerializer,RoomSerializer,DiscussionSerializer
 
 
-# Create your views here.
 
+
+# Listing available APIs
 @api_view(['GET'])
-def apiOverview(request):
+def apiList(request):
     # return JsonResponse("API BASE POINT",safe=False)
     
     api_urls = {
-		'List':'/user-list/',
-		# 'Detail View':'/task-detail/<str:pk>/',
-		# 'Create':'/task-create/',
+		'API List':'api/user-list/',
+		'Add a new user':'api/add-user',
+		'User list':'api/user-list/',
+        'Room list':'api/room-list/',
+        'Add a new room':'api/add-room',
+        'Get Discussion list':'api/disc-list/<str:pk>',
 		# 'Update':'/task-update/<str:pk>/',
 		# 'Delete':'/task-delete/<str:pk>/',
 		}
@@ -27,21 +33,7 @@ def apiOverview(request):
 
     return Response(api_urls)
 
-# authenticating users from the frontend
-@api_view(['GET'])
-def userAuth(request,pk,usr,pswrd):
-    # return JsonResponse("Task List",safe=False)
-    user = User.objects.get(id=pk)
-    
-    
-    if user.username == usr and user.password == pswrd:
-        serializer = UserSerializer(user, many=False)
-        print(user.username,user.password)
-        return Response(serializer.data)
-    else:
-        return Response({"error":"Invalid Credentials"})
-
-
+# Adding Authenticated Users by the firebase to the database
 @api_view(['GET'])
 def userList(request):
 	# return JsonResponse("Task List",safe=False)
@@ -50,7 +42,7 @@ def userList(request):
 	return Response(serializer.data)
 
 
-# Adding Authenticated Users by the firebase to the database
+
 
 @api_view(['POST'])
 def addUser(request):
@@ -59,3 +51,35 @@ def addUser(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def addRoom(request):
+    serializer = RoomSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#loading the room list
+@api_view(['GET'])
+def roomList(request):
+    # return JsonResponse("Task List",safe=False)
+    room = Room.objects.all()
+    serializer = RoomSerializer(room, many=True)
+    return Response(serializer.data)
+
+
+#loading the room list
+@api_view(['GET'])
+def discList(request,pk):
+    # return JsonResponse("Task List",safe=False)
+    discussion = Discussion
+    room = Room.objects.get(pk=pk)
+    for disc in Discussion.objects.filter(discussion_Room=room):
+        print(disc.discussion_name)
+    discussion = Discussion.objects.filter(discussion_Room=room)
+    # disscussion = discussion.objects.all()
+    # print(discussion)
+    serializer = DiscussionSerializer(discussion, many=True)
+    return Response(serializer.data)
