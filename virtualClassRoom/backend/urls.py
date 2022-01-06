7ae0d5a2c58b0fb709b9de8667f1582355517903
maazbin <maaz.mstqm@gmail.com>
2022-01-07 00:20:29 +0500

    new APIs added to API List. Database structure changed a bit. topic view created

--- virtualClassRoom/backend/urls.py
+++ virtualClassRoom/backend/urls.py
@@ -12,6 +12,9 @@ urlpatterns = [
     path('room-list', views.roomList, name='room-list'),
     path('add-room', views.addRoom, name='add-room'),
     path('disc-list/<int:pk>', views.discList, name='disc-list'),
+    path('topic-list/<int:pk>', views.topicList, name='topic-list'),
     path('user-room-list/<str:pk>', views.userRoomlList, name='user-room-list'),
     path('enrolment', views.enrolment,name='enrolment'),
+    path('rooms-of-user/<str:pk>', views.RoomsOfUser, name='rooms-of-user'),
+    path('enrol', views.enrol, name='enrol'),
 ]
--- virtualClassRoom/backend/views.py
+++ virtualClassRoom/backend/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 		# Model Imports 
-from .models import User,Room,Discussion,Enrolment
+from .models import Topic, User,Room,Discussion,Enrolment
 
         # serializers Imports
 from backend import serializers
-from .serializers import UserSerializer,RoomSerializer,DiscussionSerializer,EnrolmentSerializer
+from .serializers import UserSerializer,RoomSerializer,DiscussionSerializer,EnrolmentSerializer,TopicSerializer
 
 
 
@@ -20,14 +20,16 @@ def apiList(request):
     # return JsonResponse("API BASE POINT",safe=False)
     
     api_urls = {
-		'API List':'api/user-list/',
-		'Add a new user':'api/add-user',
-		'User list':'api/user-list/',
-        'Room list':'api/room-list/',
-        'Add a new room':'api/add-room',
-        'Get Discussion list':'api/disc-list/<int:pk>',
+		'Get API List':'api/user-list/',
+		'Get Discussion list of a room':'api/disc-list/<int:pk>',
+        'Get Topics list of a Discussion':'api/topic-list/<int:pk>',
         'Get Users in the req room' : 'api/user-room-list/<str:pk>',
-        'Enrolling a user in a group' : 'api/enrolment'
+        'Get room(s) for an enrolled specific user': 'api/rooms-of-user/<str:pk>',
+		'Get User list':'api/user-list/',
+        'Get Room list':'api/room-list/',
+        'Add a new user':'api/add-user',
+        'Add a new room':'api/add-room',
+        'Enrolling a user in a group' : 'api/enrolment',
 		}
 
 
@@ -70,39 +72,50 @@ def roomList(request):
     return Response(serializer.data)
 
 
-#List of discussions -> week in UI
+#List of discussions -> week in UI in a Room
 @api_view(['GET'])
-def discList(request,pk):
+def discList(request,pk): #pk is the room id
     discussion = Discussion
-    room = Room.objects.get(pk=pk)
-    # for disc in Discussion.objects.filter(discussion_Room=room):
-    #     print(disc.discussion_name)
-    discussion = Discussion.objects.filter(discussion_Room=room)
+    rooms = Room.objects.get(pk=pk)
+
+    discussion = Discussion.objects.filter(room=rooms)
     if discussion.count() == 0:
-        return Response("No Discussions")
+        return Response([])
    
     serializer = DiscussionSerializer(discussion, many=True)
 
     return Response(serializer.data)
+
+#List of Topic -> Topic in a Discussion
+@api_view(['GET'])
+def topicList(request,pk): #pk is the Discussion id
+    topic = Topic
+    disc = Discussion.objects.get(pk=pk)
+
+    topic = Topic.objects.filter(discussion=disc)
+    if topic.count() == 0:
+        return Response([])
+   
+    serializer = TopicSerializer(topic, many=True)
+    return Response(serializer.data)    
    
 
 # Enrolled users
 @api_view(['GET'])
 def userRoomlList(request,pk):
     enrol = Enrolment
-    # room = Room.objects.get(id=pk)
+    
     #getting data of room for a spesific user 
     enrol = Enrolment.objects.filter(room = pk)
-    # info = Room.objects.get(id=int(pk))
-    # print(info)
+    
     if enrol.count() == 0:
         return Response([])
    
     serializer = EnrolmentSerializer(enrol, many=True)
-
     return Response(serializer.data)
 
 
+# Enrol a user in a room
 @api_view(['POST'])
 def enrolment(request):
     serializer = EnrolmentSerializer(data=request.data)
@@ -119,3 +132,30 @@ def enrolment(request):
          enrol = Enrolment(room =Room.objects.get(id = 27) , student = user.objects.get(id = x.id))
          enrol.save()        
     """
+
+    
+# Returning a room(s) for a specifics user
+@api_view(['GET'])
+def RoomsOfUser(request,pk):
+#    if request.method == 'GET':
+    enrol = Enrolment
+    
+    #getting data of room for a spesific user 
+    enrol = Enrolment.objects.filter(student = pk)
+    
+    if enrol.count() == 0:
+        return Response([])
+    roomList = {}
+    for e in Enrolment.objects.filter(student = pk):
+        roomList[e.room.Room_name] = e.room.id
+    # resList = [roomList,enrol.count()]
+    return JsonResponse(roomList)
+
+
+@api_view(['GET'])
+def enrol(request):
+    enrol = Enrolment
+    enrol = Enrolment.objects.all()
+    serializer = EnrolmentSerializer(enrol,many = True)
+    return Response(serializer.data) 
+    
